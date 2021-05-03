76e2468517746387d90dcbddc78b4cb58957535e
Eti Priya <etipriya@gmail.com>
2021-05-03 22:48:38 +0100

    added name in the questions object

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const inquirer = require("inquirer");
-
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
+    name: "title",
     type: "input",
     message: "What is the title of your project?",
     reply: "Generated README.md",
   },
   {
+    name: "licence",
     type: "choices",
     message: "What is your Github Licence?",
     reply: "Bob@gmail.com",
   },
   {
+    name: "description",
     type: "input",
     message: "Write your project description",
     reply:
       "We are creating a README.md file which has multiple sections related to our project",
   },
   {
+    name: "installation",
     type: "input",
     message: "Installation",
     reply: "npm install node index.js",
   },
   {
+    name:
     type: "input",
     message: "Usage",
     reply: "When the app is running answer all questions",
@@ -70,6 +75,7 @@ const writeToFile = (fileName, data) => {};
 const init = async () => {
   const questions = [
     {
+      name: "name",
       type: "input",
       message: "What is your name?",
       reply: "Bob",
