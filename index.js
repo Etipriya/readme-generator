9375129a15b479ec429f91d96143ab89e8b08007
Eti Priya <etipriya@gmail.com>
2021-05-04 17:07:57 +0100

    added questions to init using async and await

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const questions = [
     name: "title",
     type: "input",
     message: "What is the title of your project?",
-    reply: "Generated README.md",
+    reply: "",
   },
   {
     name: "licence",
     type: "choices",
     message: "What is your Github Licence?",
-    reply: "Bob@gmail.com",
+    choices: [
+      "github.com/bob",
+      "github.com/alice",
+      "github.com/mike",
+      "github.com/steve",
+    ],
+    validate: validateInput,
   },
   {
     name: "description",
@@ -30,29 +36,33 @@ const questions = [
     reply: "npm install node index.js",
   },
   {
-    name:
+    name: "usage",
     type: "input",
     message: "Usage",
     reply: "When the app is running answer all questions",
   },
   {
+    name: "contributing",
     type: "input",
-    message: "Contributing",
+    message: "contributing",
     reply: "Open an issue or a pull request and I will sort it",
   },
   {
+    name: "tests",
     type: "input",
     message: "Testing",
     reply: "There are no tests for this app",
   },
   {
+    name: "github profile",
     type: "input",
-    message: "Github profile",
-    reply: "Link to github profile",
+    message: "link to github profile",
+    reply: "link to github profile",
   },
   {
+    name: "email-ID",
     type: "input",
-    message: "emailID",
+    message: "email-ID",
     reply: "Bob@gmail.com",
   },
 ];
@@ -66,6 +76,9 @@ const handleAnswers = answers => {
 const getAnswersFromQuestions = async questions => {
   const answers = await inquirer.prompt(questions);
   return answers;
+  //.then (answers) => {
+  //   console.log("questions", "answers"),
+  // }
 };
 
 // TODO: Create a function to write README file
@@ -75,10 +88,65 @@ const writeToFile = (fileName, data) => {};
 const init = async () => {
   const questions = [
     {
-      name: "name",
+      name: "title",
       type: "input",
-      message: "What is your name?",
-      reply: "Bob",
+      message: "What is the title of your project?",
+      reply: "",
+    },
+    {
+      name: "licence",
+      type: "choices",
+      message: "What is your Github Licence?",
+      choices: [
+        "github.com/bob",
+        "github.com/alice",
+        "github.com/mike",
+        "github.com/steve",
+      ],
+      validate: validateInput,
+    },
+    {
+      name: "description",
+      type: "input",
+      message: "Write your project description",
+      reply:
+        "We are creating a README.md file which has multiple sections related to our project",
+    },
+    {
+      name: "installation",
+      type: "input",
+      message: "Installation",
+      reply: "npm install node index.js",
+    },
+    {
+      name: "usage",
+      type: "input",
+      message: "Usage",
+      reply: "When the app is running answer all questions",
+    },
+    {
+      name: "contributing",
+      type: "input",
+      message: "contributing",
+      reply: "Open an issue or a pull request and I will sort it",
+    },
+    {
+      name: "tests",
+      type: "input",
+      message: "Testing",
+      reply: "There are no tests for this app",
+    },
+    {
+      name: "github profile",
+      type: "input",
+      message: "link to github profile",
+      reply: "link to github profile",
+    },
+    {
+      name: "email-ID",
+      type: "input",
+      message: "email-ID",
+      reply: "Bob@gmail.com",
     },
   ];
 
